97b687628f334b3f70c39a3b48978fed1f07cdc6
Juan Pablo Lorenzo <juampilorenzo@gmail.com>
2018-02-04 12:27:56 -0300

    Obtiene nombre de materia y notas

--- login.js
+++ login.js
@@ -2,17 +2,26 @@ const puppeteer = require('puppeteer');
 const CREDS = require('./creds');
 
 // Elementos del DOM
+// 1-Login
 const USUARIO_SELECTOR = '#txtUsuario';
 const PASSWORD_SELECTOR = '#pwdClave';
 const DOMINIO_SELECTOR = '#txtDominios';
 const MODO_SELECTOR = '#chk2'; // Autogestión
 const BUTTON_SELECTOR = '#btnEnviar';
+// 2-Entrada al primero
+const PRIMERA_SELECTOR = "#table-1 > tbody > tr:nth-child(1) > td:nth-child(9) > a";
+const NOTA_SELECTOR = "#ps > table:nth-child(1) > tbody > tr:nth-child(3) > td:nth-child(2) > p";
+const TIMEOUT=3000;
+
+function getNombreMateria(cadena){
+  return cadena.substr(23, cadena.length-23);
+}
 
 async function run() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   
-  // Login
+  // 1-Login
   await page.goto('https://www.frc.utn.edu.ar/logon.frc',
     {
         timeout:0
@@ -25,8 +34,39 @@ async function run() {
   await page.keyboard.type(CREDS.dominio);
   await page.click(MODO_SELECTOR);
   await page.click(BUTTON_SELECTOR);
-  await page.waitForNavigation();
-
+  //await page.waitForNavigation();
+  await page.waitFor(TIMEOUT);
+  console.log("Logueado.");
+  // 2-Entrando al primero
+  var enlace;
+  var materia;
+  var selector;
+  var nota;
+  const enlace_materia = "#iz > table > tbody > tr > td.tTit";
+  for (i=1; i<= 15; i+=2){
+    enlace = "#table-1 > tbody > tr:nth-child(" + i + ") > td:nth-child(9) > a";
+    await page.evaluate((enlace) => {
+      document.querySelector(enlace).click();
+    }, enlace);
+    await page.waitFor(TIMEOUT);
+    
+    //await page.waitForNavigation();
+    //await page.screenshot({ path: 'screenshots/primera.png' });
+    materia = await page.evaluate((enlace_materia) =>
+        document.querySelector(enlace_materia).innerText, enlace_materia);
+    materia = getNombreMateria(materia);
+    console.log(materia);
+    for (j=2; j <= 12; j++) {
+      selector = "#ps > table:nth-child(1) > tbody > tr:nth-child(3) > td:nth-child(" + j + ") > p";
+      nota = await page.evaluate((selector) => 
+        document.querySelector(selector).innerText, selector);
+      
+      if (!isNaN(nota))
+        console.log(nota);
+    }
+    await page.goBack();
+    //await page.waitFor(TIMEOUT);
+  }
   browser.close();
 }
 
---
+++ s
@@ -0,0 +1,11 @@
+#ps > table:nth-child(1) > tbody > tr:nth-child(3) > td:nth-child(2) > p
+#ps > table:nth-child(1) > tbody > tr:nth-child(3) > td:nth-child(3) > p
+#ps > table:nth-child(1) > tbody > tr:nth-child(3) > td:nth-child(4) > p
+#ps > table:nth-child(1) > tbody > tr:nth-child(3) > td:nth-child(5) > p
+
+
+#ps > table:nth-child(1) > tbody > tr:nth-child(3) > td:nth-child(12) > p
+
+for (i=1; i <= 12; i++) {
+    "#ps > table:nth-child(1) > tbody > tr:nth-child(3) > td:nth-child(" + i + ") > p"
+}"#ps > table:nth-child(1) > tbody > tr:nth-child(3) > td:nth-child(2) > p"
